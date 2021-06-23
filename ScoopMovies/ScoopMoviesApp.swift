308f4271f939d48f5173a8deda768d309e4124b9
aakash sinha <aksinha2509@gmail.com>
2021-06-24 00:37:50 +0530

    Initial Commit

---
+++ ScoopMovies/ScoopMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoopMoviesApp.swift
+//  ScoopMovies
+//
+//  Created by Aakash Sinha on 24/06/21.
+//
+
+import SwiftUI
+
+@main
+struct ScoopMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
